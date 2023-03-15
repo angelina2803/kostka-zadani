4d2b326669ebb7d339c5b1a7bf88cd35378e6dee
Angelina Yumatova <>
2023-03-15 16:34:34 +0100

    change

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('funguju!');
+let side = 1
+const kostka = document.querySelector('.dice')
+const pootocKostkou = () => {
+	side = side + 1
+	if (side === 7) {
+		side = 1
+	}
+	kostka.src = `img/side${side}.svg`
+}
+document.addEventListener('keydown', pootocKostkou)
+
